5c788d217d7bb03d5ef9b3394074755156165f54
Hinata-bot-00 <lolajunior670@gmail.com>
2024-09-09 14:24:14 +0100

    Create ha08.js

---
+++ scripts/cmds/ha08.js
@@ -0,0 +1,20 @@
+module.exports = {
+	config: {
+			name: "😍", 
+			version: "1.0",
+			author: "Jaychris Garcia",
+			countDown: 5,
+			role: 0,
+			shortDescription: "sarcasm",
+			longDescription: "sarcasm",
+			category: "reply",
+	},
+onStart: async function(){}, 
+onChat: async function({
+	event,
+	message,
+	getLang
+}) {
+	if (event.body && event.body.toLowerCase() == "😍") return message.reply("˚∧＿∧  　+        —̳͟͞͞💗\n(  •‿• )つ  —̳͟͞͞ 💗         —̳͟͞͞💗 +\n(つ　 <                —̳͟͞͞💗\n｜　 _つ      +  —̳͟͞͞💗         —̳͟͞͞💗 ˚\n`し´");
+}
+};
