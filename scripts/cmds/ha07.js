e8c8e7e3d6dace4d8de9af28b8a67aade0c06946
Hinata-bot-00 <lolajunior670@gmail.com>
2024-09-09 12:49:42 +0100

    Create ha07.js

---
+++ scripts/cmds/ha07.js
@@ -0,0 +1,20 @@
+module.exports = {
+	config: {
+			name: "🥰",
+			version: "1.0",
+			author: "Jaychris Garcia",
+			countDown: 5,
+			role: 0,
+			shortDescription: "sarcasm",
+			longDescription: "sarcasm",
+			category: "reply",
+	},
+onStart: async function(){}, 
+onChat: async function({
+	event,
+	message,
+	getLang
+}) {
+	if (event.body && event.body.toLowerCase() == "🥰") return message.reply("❤️");
+}
+};
